47fe7e0d63b61542fcf33ca377556f6a1eceb1d0
callumsmith101 <40399574@live.napier.ac.uk>
2020-02-10 15:17:25 +0000

    Commit 25. Prints list of salaries.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -154,6 +154,20 @@ public class App
         }
     }
 
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+    }
+
     public static void main(String[] args)
     {
         // Create new Application
@@ -164,6 +178,8 @@ public class App
 
         // Extract employee salary information
         ArrayList<Employee> employees = a.getAllSalaries();
+
+        a.printSalaries(employees);
 
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
